7d65e70d68ba3eeb09b349f70f23cce23641f4d1
a.shoeibi <a.shoeibi@DESKTOP-TPOSAII>
2021-07-09 13:45:32 +0430

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlockingCollectionDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using BlockingCollection<int> numbersBlockingCollection = new(new ConcurrentQueue<int>());
+
+            IEnumerable<int> fromOneToTwentyFiveThousand = Enumerable.Range(1, 25000);
+
+            Task firstEnqueuingTask = Task.Run(async () =>
+           {
+               foreach (var number in fromOneToTwentyFiveThousand)
+               {
+                   numbersBlockingCollection.Add(number);
+                   Console.WriteLine($"{number} is Enqueued");
+                   
+               }
+               numbersBlockingCollection.CompleteAdding();
+           });
+
+            Task firstDequeuingTask = Task.Run(async () =>
+            {
+                foreach (var number in numbersBlockingCollection.GetConsumingEnumerable())
+                {
+                    Console.WriteLine($"{number} is Dequeued");
+                }
+
+            });
+            await Task.WhenAll(firstEnqueuingTask, firstDequeuingTask);
+
+        }
+    }
+}
